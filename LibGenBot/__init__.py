dd3670284ba7d308ae1fc6331fdbb498a2917d4e
adenosinetp10 <adenosinetp10@gmail.com>
2023-01-29 14:59:34 +0530

    using `dotenv` now

--- LibGenBot/__init__.py
+++ LibGenBot/__init__.py
@@ -1,7 +1,10 @@
 from telegram.ext import Updater
 import os
+from dotenv import load_dotenv
 
-TOKEN = os.environ.get('bot_token')
+load_dotenv()
+
+TOKEN = os.environ.get("bot_token", "")
 
 updater = Updater(TOKEN)
 dp = updater.dispatcher
